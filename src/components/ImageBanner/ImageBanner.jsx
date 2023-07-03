8194d0e15297f05ea47bf38a624dcc415b1eb701
Cyril Dufour <titi.cloduf@gmail.com>
2023-07-03 22:21:06 +0200

    new 03/07/2023

--- src/components/ImageBanner/ImageBanner.jsx
+++ src/components/ImageBanner/ImageBanner.jsx
@@ -1,37 +1,42 @@
-import React, { useState } from 'react'
-import './ImageBanner.css'
+import React, { useState } from 'react';
+import './ImageBanner.css';
 
 function ImageBanner(props) {
   const pictures = props.pictures;
-  const [photoActuel, photoActuelFixe] = useState(0);
+  const [photoActuel, setPhotoActuel] = useState(0);
+
   const getClassName = (i) => {
     if (i === photoActuel) return "show";
     return "";
   };
 
   const photoSuivante = () => {
-    photoActuelFixe((photoActuel + 1) % pictures.length);
-  }
+    setPhotoActuel((photoActuel + 1) % pictures.length);
+  };
 
   const photoPrecedente = () => {
     const newphotoActuel = photoActuel - 1;
     if (newphotoActuel < 0) {
-      photoActuelFixe(pictures.length - 1)
+      setPhotoActuel(pictures.length - 1);
       return;
     }
-    photoActuelFixe(photoActuel - 1);
-  }
+    setPhotoActuel(photoActuel - 1);
+  };
 
   return (
     <div className='image__banner'>
-     
-      {pictures.map((pic, i) => <img key={pic} src={pic} alt="" className={getClassName(i)}></img>)}
-      <div className='btn'>
-      <button className='btn-suivant' onClick={photoSuivante}><i class="fa-sharp fa-solid fa-chevron-left"></i></button>
-      <button className='btn-precedent' onClick={photoPrecedente}><i class="fa-sharp fa-solid fa-chevron-right"></i></button>
-      </div>
+      {pictures.map((pic, i) => (
+        <img key={pic} src={pic} alt="" className={getClassName(i)}></img>
+      ))}
+      {pictures.length > 1 && (
+        <div className='btn'>
+          <button className='btn-suivant' onClick={photoSuivante}><i class="fa-sharp fa-solid fa-chevron-left"></i></button>
+          <button className='btn-precedent' onClick={photoPrecedente}><i class="fa-sharp fa-solid fa-chevron-right"></i></button>
+        </div>
+      )}
+      <div className='numerotation'>{`${photoActuel + 1}/${pictures.length}`}</div>
     </div>
-  )
+  );
 }
 
-export default ImageBanner
+export default ImageBanner;
